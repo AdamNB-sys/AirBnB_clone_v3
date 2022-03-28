9cf8f689ead2906a224130bb3404427bfa59c2bb
AdamNB-sys <a1adnat@gmail.com>
2022-03-28 13:41:40 -0500

    committing task 4 progress to test in python 3.4 env

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""this initializes the views of api v1"""
+from api.v1.views.index import *
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
